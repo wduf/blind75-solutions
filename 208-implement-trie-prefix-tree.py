1a939975e5a56b62de5258baec3d610e7a584843
will dufault <99445180+wduf@users.noreply.github.com>
2023-01-24 10:41:47 -0500

    Update 208-implement-trie-prefix-tree.py

--- 208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree.py
@@ -1,3 +1,53 @@
+# without nodes
+# 73.37% time
+# 78.70% memory
+
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+
+    def insert(self, word: str) -> None:
+        ln = len(word)
+        
+        def f(d: dict, i: int) -> None:
+            if i == ln:
+                d[0] = True
+                return
+            c = word[i]
+            if c not in d:
+                d[c] = {}
+            f(d[c], i + 1)
+        
+        f(self.children, 0)
+
+    def search(self, word: str) -> bool:
+        ln = len(word)
+        
+        def f(d: dict, i: int) -> bool:
+            if i == ln:
+                return 0 in d
+            c = word[i]
+            if c not in d:
+                return False
+            return f(d[c], i + 1)
+    
+        return f(self.children, 0)
+
+    def startsWith(self, pre: str) -> bool:
+        ln = len(pre)
+        
+        def f(d: dict, i: int) -> bool:
+            if i == ln:
+                return True
+            c = pre[i]
+            if c not in d:
+                return False
+            return f(d[c], i + 1)
+    
+        return f(self.children, 0)
+
+# with nodes
 # 45.05% time
 # 15.63% memory
 
