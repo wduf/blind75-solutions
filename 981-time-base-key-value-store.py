09383a1dc6ee1c4a2f46f42d2e03f221a6c1dcd2
will dufault <99445180+wduf@users.noreply.github.com>
2023-01-30 21:01:23 -0500

    Update 981-time-base-key-value-store.py

--- 981-time-base-key-value-store.py
+++ 981-time-base-key-value-store.py
@@ -1,3 +1,24 @@
+# 99.35% time
+# 91.12% memory
+
+class TimeMap:
+
+    def __init__(self):
+        self.d = defaultdict(lambda: defaultdict(int))
+
+    def set(self, k: str, v: str, t: int) -> None:
+        self.d[k][t] = v
+
+    def get(self, k: str, t: int) -> str:
+        cur_d = self.d[k]  # cur dict
+        # NOTE: python dicts keep order of insertion, so we can iterate
+        #       backwards through dict until largest time <= t is found
+        for x in reversed(cur_d):
+            if x <= t:
+                return cur_d[x]
+        # smaller than smallest val in cur dict
+        return ""
+
 # 98.10%
 # 88.15%
 
